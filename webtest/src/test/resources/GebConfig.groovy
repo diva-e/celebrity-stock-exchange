7ab5df64154af9a0bdc8fd68bcb1e0d3ae31825f
Elias Bordolo <ebordolo@netpioneer.de>
2019-09-27 12:51:38 +0200

    change where local webdrivers are stored; add config for remote webdrivers

--- webtest/src/test/resources/GebConfig.groovy
+++ webtest/src/test/resources/GebConfig.groovy
@@ -1,5 +1,11 @@
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.RemoteWebDriver
 
+// points to local driver, maybe not needed
+System.setProperty('webdriver.chrome.driver', 'src/test/resources/drivers')
 
-System.setProperty('webdriver.chrome.driver', 'src/test/resources/chromedriver')
 baseUrl = "https://www.diva-e.com/"
 reportsDir = "${System.getenv('HOME')}/webtest-reporting"
+
+hubUrl = "http://34.77.204.251:4444/wd/hub"
+driver = {new RemoteWebDriver(new URL("${hubUrl}"), DesiredCapabilities.chrome())}
